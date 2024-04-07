d361900eebbeb137a802e6b8ece18424c5897bcf
KennnedyRamos <kennedy_ramos9@icloud.com>
2024-04-06 22:46:09 -0300

    Subindo postifolio

---
+++ src/js/index.js
@@ -0,0 +1,17 @@
+const botaoMostrarProjetos = document.querySelector('.btn-mostrar-projetos');
+const projetosInativos = document.querySelectorAll('.projeto:not(.ativo)');
+
+botaoMostrarProjetos.addEventListener('click', () => {
+    mostrarMaisProjetos();
+    esconderBotao();
+});
+
+function esconderBotao() {
+    botaoMostrarProjetos.classList.add("remover");
+}
+
+function mostrarMaisProjetos() {
+    projetosInativos.forEach(projetoInativo => {
+        projetoInativo.classList.add('ativo');
+    });
+}
